4c7a97fac8a0056e17fff175390374fe4f2e8a8f
Talha Çağatay IŞIK <talhacagatayisik@gmail.com>
2018-08-21 22:02:45 +0300

    Pillars of OOP
    A great example for Pillars of OOP

---
+++ OOPExamples/PowerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExamples
+{
+    class PowerController : IPowerOn
+    {
+        public bool isOn
+        {
+            get;
+            private set;
+        }
+
+        protected void TurnOn()
+        {
+            isOn = true;
+        }
+
+        protected void TurnOff()
+        {
+            isOn = false;
+        }
+
+        public virtual void TogglePower()
+        {
+            if (isOn)
+            {
+                TurnOff();
+            }
+            else
+            {
+                TurnOn();
+            }
+        }
+    }
+}
---
+++ OOPExamples/Program.cs
@@ -0,0 +1,36 @@
+﻿using OOPExamples;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //var comp = new Desktop("Generic Computer",Desktop.CaseType.Tower);
+
+        var comp = new Laptop("Generic Laptop", 1024, 768);
+
+        Console.WriteLine("Computer type : " + comp.GetType());
+
+        var isComputer = comp is Computer;
+
+        Console.WriteLine("is Computer : " + isComputer);
+        Console.WriteLine("Computer name : " + comp.name);
+        //Console.WriteLine("Computer case : " + comp.caseType);
+
+        comp.TogglePower();
+        Console.WriteLine("Computer is on : " + comp.isOn);
+
+        var sleepingComp = comp as ISleep;
+
+        if (sleepingComp != null)
+        {
+            sleepingComp.ToggleSleep();
+            Console.WriteLine("Computer is sleeping : " + sleepingComp.isSleeping);
+
+            comp.TogglePower();
+            Console.WriteLine("Computer is on : " + comp.isOn);
+        }
+
+        Console.Read();
+    }
+}
